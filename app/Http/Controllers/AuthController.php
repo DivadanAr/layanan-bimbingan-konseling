4d4a18a19d7069fe70fe4e6337b2850670792f5b
Rasya09 <rasyafadil09@gmail.com>
2023-06-13 10:01:53 +0700

    api login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,12 +11,12 @@ class AuthController extends Controller
 {
     public function register(Request $req)
     {
-        $rules=[
-            'name'=> 'required',
-            'email'=> 'required',
-            'password'=> 'required',
+        $rules = [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
         ];
-        
+
         $validator = Validator::make($req->all(), $rules);
 
         if ($validator->fails()) {
@@ -24,38 +24,30 @@ class AuthController extends Controller
         }
 
         $user = User::create([
-            'name'=>$req->name,
-            'email'=>$req->email,
-            'password'=>Hash::make($req->password),
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password),
         ]);
 
         $token = $user->createToken('Personal Access Token')->plainTextToken;
 
-        $response = ['user'=>$user, 'token'=>$token];
+        $response = ['user' => $user, 'token' => $token];
 
         return response()->json($response, 200);
-    }    
+    }
 
-    public function login(Request $req)
+    function Login(Request $R)
     {
-        $rules=[
-            'email'=> 'required',
-            'password'=> 'required',
-        ];
-        
-        $req->validate($rules);
+        $user = User::where('name', $R->name)->first();
 
-        $user = User::where('email', $req->email)->first();
-
-        if ($user && Hash::check    ($req->password, $user->password)) {
-            $token = $user->createToken('Personal Acces Token')->plainTextToken;
-            $response = ['user'=>$user, 'token'=>$token];
-
-            return response()->json($response, 200);
+        if ($user != 'siswa' && Hash::check($R->password, $user->password)) {
+            $token = $user->createToken('Personal Access Token')->plainTextToken;
+            $response = ['status' => 200, 'token' => $token, 'user' => $user, 'message' => 'Login Successfully!'];
+            return response()->json($response);
+        } else if ($user == 'siswa') {
+            $response = ['status' => 500, 'message' => 'No account found with this username'];
+        } else {
+            $response = ['status' => 500, 'message' => 'Wrong username or password! please try again'];
         }
-
-        $response = ['message'=>'Incorrect Email or Password'];
-
-        return response()->json($response, 400);
-    }    
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::post('auth/register', [AuthController::class, 'register']);
 
-Route::post('auth/login', [AuthController::class, 'login']);
+Route::post('auth/login', [AuthController::class, 'Login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
